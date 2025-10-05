3ef3908186db3075a985550f3ef8e6e5c078274f
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-10-05 19:19:35 -0300

    first commit

---
+++ src/main/java/com/example/MonoGestor/MonoGestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.MonoGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MonoGestor/MonoGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MonoGestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
